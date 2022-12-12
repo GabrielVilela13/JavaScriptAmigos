0e58900321d4f1ab7fb6261dcc4a5ca5149d871d
GabrielVilela13 <115550288+GabrielVilela13@users.noreply.github.com>
2022-12-12 10:10:54 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let nome="Gabriel";
+console.log(nome)
+let idade= 18;
+console.log(idade)
+console.log(idade*7)
+
+let amigos =[
+    {
+        nome: "David",
+        idade: 18,
+        éestudante: true
+    },
+    {
+        nome: "Pedro",
+        idade: 19,
+        éestudante: false
+    },
+    {
+        nome: "André",
+        idade: 20,
+        éestudante: true
+    }
+];
+console.log(amigos);
+
